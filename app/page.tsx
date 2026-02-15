e09771fe2bf23e703e2969d1e7784544a9ef3047
Ayush Badola <ayush.badola.04@gmail.com>
2026-02-15 14:45:45 +0530

    feat: small changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,28 @@ import ChatInputArea from '@/components/manual/chat-input-area'
 import PageHeader from '@/components/manual/page-header'
 import { Message, Role } from '@/validation/types'
 import { useState } from 'react'
-import { AnimatePresence, motion } from 'motion/react'
+import { AnimatePresence, easeOut, motion } from 'motion/react'
 import InititalPage from '@/components/manual/initial-page'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { MessageFormData, messageSchema } from '@/validation/schema'
 import { zodResolver } from '@hookform/resolvers/zod'
+import Mascot from '@/components/manual/mascot'
+
+const initialMessageAnimation = {
+  opacity: 0,
+  y: -20,
+  filter: 'blur(6px)',
+}
+
+const animateMessageTo = {
+  opacity: 1,
+  y: 0,
+  filter: 'blur(0px)',
+  transition: {
+    duration: 0.35,
+    ease: easeOut,
+  },
+}
 
 export default function ChatInterfacePage() {
   const [messages, setMessages] = useState<Message[]>([])
@@ -21,44 +38,76 @@ export default function ChatInterfacePage() {
     resolver: zodResolver(messageSchema),
   })
 
-  function handleSendMessage(content: string) {
+  function handleSendMessage({ message }: MessageFormData) {
     const newUserMessage: Message = {
       role: Role.USER,
-      content,
+      content: message,
     }
     setMessages((prevMessages) => [...prevMessages, newUserMessage])
     setConversationStarted(true)
-
+    messageForm.resetField('message')
+    messageForm.setFocus('message')
     setTimeout(() => {
       const assistantResponse: Message = {
         role: Role.ASSISTANT,
-        content: `This is a simulated response to the earlier message provided by  the user. The content of the user message was following ->\n ${content}`,
+        content: `This is a simulated response to the earlier message provided by  the user. The content of the user message was following ->\n ${message}`,
       }
       setMessages((prevMessages) => [...prevMessages, assistantResponse])
     }, 3000)
   }
 
-  const onSubmit: SubmitHandler<MessageFormData> = (data) => {
-    console.log(data)
-    // handleSendMessage(data.message)
+  function renderMessage(msg: Message, index: number) {
+    if (msg.role === Role.USER) {
+      return (
+        <motion.div
+          key={index}
+          initial={initialMessageAnimation}
+          animate={animateMessageTo}
+          className="bg-accent text-accent-foreground mt-12 mb-4 max-w-[80%] self-end rounded-lg px-4 py-2 shadow-sm"
+        >
+          {msg.content}
+        </motion.div>
+      )
+    }
+
+    return (
+      <motion.div
+        key={index}
+        initial={initialMessageAnimation}
+        animate={animateMessageTo}
+        className="text-foreground self-start"
+      >
+        {msg.content}
+      </motion.div>
+    )
   }
-
-
   return (
     <div className="flex h-screen flex-col items-center">
       <PageHeader />
       <AnimatePresence mode="wait">
         {conversationStarted ? (
-          <motion.div key="chat" className="h-full w-full bg-red-100">
+          <motion.div
+            key="chat"
+            className="relative flex h-full w-full justify-center p-4"
+          >
+            {/* messages area */}
+            <motion.div layout className="flex w-full max-w-2xl flex-col px-2">
+              {messages.map((msg, index) => renderMessage(msg, index))}
+              <motion.div layout>
+                <Mascot className="size-12 self-start" />
+              </motion.div>
+              <div className="min-h-[25rem]" />
+            </motion.div>
+
             <motion.div
               layoutId="chat-input-area"
               className="fixed bottom-0 w-full p-2"
             >
-              <ChatInputArea {...messageForm} onSubmit={onSubmit} />
+              <ChatInputArea {...messageForm} onSubmit={handleSendMessage} />
             </motion.div>
           </motion.div>
         ) : (
-          <InititalPage form={messageForm} onSubmit={onSubmit} />
+          <InititalPage form={messageForm} onSubmit={handleSendMessage} />
         )}
       </AnimatePresence>
     </div>
--- src/components/manual/chat-input-area.tsx
+++ src/components/manual/chat-input-area.tsx
@@ -53,7 +53,7 @@ export default function ChatInputArea({
       onSubmit={handleSubmit(onSubmit)}
       className="mx-auto max-w-2xl px-3 py-2 sm:px-4 md:px-6"
     >
-      <InputGroup className="bg-card flex w-full flex-col rounded-2xl p-2 shadow-sm not-dark:bg-white!">
+      <InputGroup className="bg-card flex w-full flex-col rounded-2xl p-2 shadow-sm not-dark:bg-white! dark:bg-popover">
         <InputGroupTextarea
           className="text-card-foreground scrollbar field-sizing-content max-h-60 w-full text-base!"
           placeholder="How can I help today?"
